e045b5db53797ef36962e7171c8a77e1b870af6d
Leledumbo Dev <leledumbo549@gmail.com>
2019-02-07 11:47:54 +0700

    release

---
+++ precache-manifest.83d54e87d4949be008fe78ad49a4bc56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c83a9fbe57d8258004d7",
+    "url": "/static/js/main.c83a9fbe.chunk.js"
+  },
+  {
+    "revision": "9b9392e6ce3d6d925e37",
+    "url": "/static/js/1.9b9392e6.chunk.js"
+  },
+  {
+    "revision": "c83a9fbe57d8258004d7",
+    "url": "/static/css/main.1050f89a.chunk.css"
+  },
+  {
+    "revision": "4b358c2f2d748c531868ad17b50354ee",
+    "url": "/index.html"
+  }
+];
